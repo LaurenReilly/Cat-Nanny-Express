de4856c0751a11d74c7233e057607c4c18d55d85
Ashley Souvannaraj <ashleysouvannaraj@Asouv.local>
2018-11-27 20:58:08 -0500

    UGH

--- win-screen.js
+++ win-screen.js
@@ -13,20 +13,18 @@ scoreContainer.innerHTML = score;
 
 //play again button
 function playAgain() {
-    window.location = "sign-in.html";
+   window.location = "sign-in.html";
 }
 
 //download information from firebase and display it
 function highScoreList() {
 
-    var highScore = firebase.database().ref('highScores/').orderByChild('highScores/finalScore').limitToFirst(10);
+   var highScore = firebase.database().ref('highScores/').orderByChild('highScores/finalScore').limitToFirst(10);
 
-    var nameFromDb = entry.val().name;
-    var scoreFromDb = entry.val().finalScore
-    highScore = `
-    <div class="row border-bottom mt-3">
-        <div class="col" style="font-size: 22px;" id="username">${nameFromDb}</div>
-        <div class="col" style="font-size: 22px;" id="highscores">${scoreFromDb}</div>
-    </div> `
-
-}
+   var nameFromDb = entry.val().name;
+   var scoreFromDb = entry.val().finalScore
+   highScore = `
+   <div class="row border-bottom mt-3">
+       <div class="col" style="font-size: 22px;" id="username">${nameFromDb}</div>
+       <div class="col" style="font-size: 22px;" id="highscores">${scoreFromDb}</div>
+   </div> `
