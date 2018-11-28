87ecf0ebf2c00c627f967cd77abcba7561d98a11
Ashley Souvannaraj <ashleysouvannaraj@Asouv.local>
2018-11-27 20:52:32 -0500

    updating changes to win-screen

---
+++ win-screen.js
@@ -0,0 +1,32 @@
+var database = firebase.database();
+
+var highScoreContainer = document.querySelector('highScoreContainer');
+var highScore = firebase.database().ref('highScores/');
+
+//get username and score from local storage
+var userName = localStorage.getItem("name");
+var score = localStorage.getItem("score");
+
+//putting the score on the page
+var scoreContainer = document.getElementById("scoreContainer");
+scoreContainer.innerHTML = score;
+
+//play again button
+function playAgain() {
+    window.location = "sign-in.html";
+}
+
+//download information from firebase and display it
+function highScoreList() {
+
+    var highScore = firebase.database().ref('highScores/').orderByChild('highScores/finalScore').limitToFirst(10);
+
+    var nameFromDb = entry.val().name;
+    var scoreFromDb = entry.val().finalScore
+    highScore = `
+    <div class="row border-bottom mt-3">
+        <div class="col" style="font-size: 22px;" id="username">${nameFromDb}</div>
+        <div class="col" style="font-size: 22px;" id="highscores">${scoreFromDb}</div>
+    </div> `
+
+}
