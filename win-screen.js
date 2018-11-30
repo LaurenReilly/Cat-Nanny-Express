f4b4a3142fb7f1a23ec5b1a68bae2c8f7cc64f4f
Ashley Souvannaraj <ashleysouvannaraj@Asouv.local>
2018-11-29 21:03:07 -0500

    saucy

--- win-screen.js
+++ win-screen.js
@@ -20,7 +20,7 @@ function playAgain() {
 //download information from firebase and display it
 function highScoreList() {
 
-   var scoresRef = firebase.database().ref('highScores').orderByChild('/finalScore').limitToLast(7);
+   var scoresRef = firebase.database().ref('highScores').orderByChild('/finalScore').limitToLast(5);
 
    scoresRef.once('value', function(snapshot) {
     var ourArray = [];
@@ -44,8 +44,8 @@ function highScoreList() {
     ourArray.forEach(function(item){
         var highScore = `
         <div class="row border-bottom mt-3">
-            <div class="col" style="font-size: 22px;" id="username">${item.name}</div>
-            <div class="col" style="font-size: 22px;" id="highscores">${item.finalScore}</div>
+            <div class="col" style="font-size: 22px; color:#DFDCE3; font-family: Montserrat" id="username">${item.name}</div>
+            <div class="col" style="font-size: 22px; color:#DFDCE3; font-family: Montserrat" id="highscores">${item.finalScore}</div>
         </div> ` 
 
         document.getElementById('highScoreList').innerHTML += highScore;
